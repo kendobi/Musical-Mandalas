4bf6432bc046c87866d49627247334f616d39847
Kenny Kong <kenny@n3twork.com>
2017-03-24 13:18:25 -0700

    update

---
+++ Assets/NotifyScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotifyScene : MonoBehaviour {
+
+	public GameObject go;
+	public string val;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		go.SendMessage ("ActivateBool", val);
+	
+	}
+}
---
+++ Assets/SceneController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+	public static bool a;
+	public static bool b;
+	public static bool c;
+
+	Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		a = false;
+		b = false;
+		c = false;
+
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void ActivateBool(int holder){
+		if (holder == 1) {
+			anim.SetBool ("a", true);
+		}
+		if (holder == 2) {
+			anim.SetBool ("b", true);
+		}
+		if (holder == 3) {
+			anim.SetBool ("c", true);
+		}
+
+
+	}
+}
+
