7b3a0c68d96038abc1098298a19078519e8b6bf1
Kenny Kong <kenny@n3twork.com>
2017-07-25 17:01:33 -0700

    menu system

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour 
+{
+
+	public void SceneButton (string Level){
+		SceneManager.LoadScene (Level);
+	}
+
+}
