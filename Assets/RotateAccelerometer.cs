fc0070d938517c26b27a02efc9c8a6bf8f5a9710
Kenny Kong <kenny@n3twork.com>
2017-08-21 10:08:30 -0700

    Updates

---
+++ Assets/RotateAccelerometer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAccelerometer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+		Vector3 dir = Vector3.zero;
+		dir = Input.acceleration.normalized;
+		dir.y = 0;
+		dir.z = 0;
+		if (dir.x >= 0.3) {
+			dir.x = 0.3f;
+		} else if (dir.x <= -0.3) {
+			dir.x = -0.3f;
+		}
+		Debug.Log (dir);
+		transform.up = dir.normalized;
+	}
+}
