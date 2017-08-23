075933836072b4c123ed8dc9c9e1fb3b4c4a0863
Kenny Kong <kenny@n3twork.com>
2017-08-23 01:59:23 -0700

    Updates

--- Assets/GrowMat.cs
+++ Assets/GrowMat.cs
@@ -4,7 +4,17 @@ using System.Collections;
 public class GrowMat : MonoBehaviour {
 
 	public GameObject obj;
+	public float speedOn = 0.5f;
+	public float speedOff = 0.5f;
 	Renderer mat;
+
+	private float min = 0.0f;
+	private float max = 1.0f;
+	private float myopacity = 0.0f;
+	private float currOpacity = 0.0f;
+	private float t = 0.0f;
+
+	private bool isTriggered = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,12 +23,38 @@ public class GrowMat : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+
+		if (isTriggered) {
+
+
+			
+			myopacity = Mathf.Lerp (currOpacity,max, t);
+			t += speedOn * Time.deltaTime;
+
+		} else if (!isTriggered && myopacity>0.1f) {
+
+			
+			myopacity = Mathf.Lerp (currOpacity,min, t);
+			t += speedOff * Time.deltaTime;
+		}
+
+		mat.material.SetFloat ("_opacity", myopacity);
+
 	}
 
 	void OnTriggerEnter(Collider other){
-		mat.material.color = new Color(0f, 0f, 0f, 1f);
+		
+		isTriggered = true;
+		t = 0.0f;
+		currOpacity = myopacity;
 
+	}
+
+	void OnTriggerExit(Collider other){
+		isTriggered = false;
+		t = 0.0f;
+		currOpacity = myopacity;
 	}
 
 
